18a634e5135d9d44d21e72bba78b50d67b90ab83
EdBomond <eduard.morgasov@yandex.ru>
2024-03-12 07:52:40 +0500

    My first commit

---
+++ file1.py
@@ -0,0 +1,18 @@
+cars=['audi','bmw','lada','sangyong','fiat','seat','tatra','kamaz']
+for car in sorted(cars[0:3]):
+    if car=='bmw':
+    	print(car.upper())
+    else:
+        print(car.title())	
+if 'audi1' in cars:
+    print('Да, это автомобиль')
+else:
+    print('Нет, это не автомобиль')
+if 'audi1' in cars:
+	print("Yes")
+elif 'bmw' in cars:
+    print("Bmw")
+elif 'lada' in cars:
+    print("lada")    
+else:
+    print("No")    	
