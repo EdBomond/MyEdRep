660885b88a0bb96ba5c96fd0f0b97b94701dec75
EdBomond <eduard.morgasov@yandex.ru>
2024-03-12 14:54:03 +0500

    Third commit

--- file1.py
+++ file1.py
@@ -1,9 +1,9 @@
 cars=['audi','bmw','lada','sangyong','fiat','seat','tatra','kamaz']
 for car in sorted(cars[0:3]):
     if car=='bmw':
-    	print(car.upper())
-    else:
-        print(car.title())	
+      print(car.upper())
+    else: 
+      print(car.title())	
 if 'audi1' in cars:
     print('Да, это автомобиль')
 else:
--- file2.py
+++ file2.py
@@ -1,3 +1,7 @@
 print("Всем привет!")
 name_="всем привет"
 print( name_.title() )
+f_name="Frank"
+s_name="Gerbert"
+full_name=f"{f_name} {s_name}"
+print( full_name.title() )
